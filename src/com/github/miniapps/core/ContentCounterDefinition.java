472dc12c76436cf9b9294e80936b8fed5089525b
Fábio Lima <fabiolimace@gmail.com>
2014-11-11 10:53:21 -0300

    Create ContentCounterDefinition.java

---
+++ src/com/github/miniapps/core/ContentCounterDefinition.java
@@ -0,0 +1,9 @@
+package com.github.miniapps.core;
+
+public class ContentCounterDefinition extends CounterDefinition {
+
+	public ContentCounterDefinition() {
+	// TODO Auto-generated constructor stub
+	}
+
+}
